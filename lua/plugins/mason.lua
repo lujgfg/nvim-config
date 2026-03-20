202f4d4153bb569f51d7452046c0ef8ad881fafa
Lu JingFeng <jingfengggggg@gmail.com>
2026-03-20 14:44:22 +0800

    fix<.>: now can used in linux

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,45 +1,101 @@
+--return {
+--    "mason-org/mason.nvim",
+--    event = "VeryLazy",
+--    dependencies = {
+--        "neovim/nvim-lspconfig",
+--        "mason-org/mason-lspconfig.nvim"
+--    },
+--    opts = {
+--      ui = {
+--        icons = {
+--            package_installed = "✓",
+--            package_pending = "➜",
+--            package_uninstalled = "✗"
+--        }
+--      }
+--    },
+--    config = function (_, opts)
+--        require("mason").setup(opts)
+--        local registry = require "mason-registry"
+--
+--        local function setup(name, config)
+--          local ok, pkg = pcall(registry.get_package, name)
+--          if  ok  and not pkg:is_installed() then
+--            pkg:install()
+--          end
+--
+--          local lsp = require("mason-lspconfig").get_mappings().package_to_lspconfig[name]
+--          vim.lsp.config(lsp, config)
+--        end
+--
+--        setup("lua-language-server", {
+--          settings = {
+--            Lua = {
+--              diagnostics = {
+--                globals = { "vim" }
+--              },
+--            }
+--          }
+--        })
+--
+--        setup("pyright", {})
+--
+--        vim.diagnostic.config({ update_in_insert = true })
+--    end,
+--}
+
 return {
+  {
     "mason-org/mason.nvim",
     event = "VeryLazy",
-    dependencies = {
-        "neovim/nvim-lspconfig",
-        "mason-org/mason-lspconfig.nvim"
-    },
     opts = {
       ui = {
         icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-      }
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
     },
-    config = function (_, opts)
-        require("mason").setup(opts)
-        local registry = require "mason-registry"
+  },
 
-        local function setup(name, config)
-          local ok, pkg = pcall(registry.get_package, name)
-          if  ok  and not pkg:is_installed() then
-            pkg:install()
-          end
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = { "neovim/nvim-lspconfig" },
 
-          local lsp = require("mason-lspconfig").get_mappings().package_to_lspconfig[name]
-          vim.lsp.config(lsp, config)
-        end
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "pyright",
+        "bashls",
+        "jsonls",
+        "yamlls",
+        "r_language_server",
+      },
+      automatic_enable = true,
+    },
 
-        setup("lua-language-server", {
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = { "vim" }
-              },
-            }
-          }
-        })
+    config = function(_, opts)
+      require("mason-lspconfig").setup(opts)
 
-        setup("pyright", {})
+      -- 自定义配置
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      })
 
-        vim.diagnostic.config({ update_in_insert = true })
+      vim.diagnostic.config({
+        update_in_insert = true,
+      })
     end,
+  },
 }
+
+
+
+
